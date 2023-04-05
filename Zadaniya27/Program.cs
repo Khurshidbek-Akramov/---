6d7808d2f864994d3c42208d4c9e285b6fd238cf
Khurshidbek-Akramov <verov.uz.1202@gmail.com>
2023-04-05 06:30:30 +0500

    Domashbaya zadaniya vipolnena

---
+++ Zadaniya27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int SumNumbers = 0;
+
+for (int i = 0; i < number.Length; i++)
+{
+    string str = Convert.ToString(number[1]);
+    int number1 = Convert.ToInt32(str);
+    SumNumbers = SumNumbers + number1;
+}
+Console.Write(SumNumbers);
